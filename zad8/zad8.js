26824fe1e63a7566b791099f8dfb5654fc7b954e
fnowak <filip.nowak7@gmail.com>
2024-07-02 18:28:36 +0200

    zad8

---
+++ zad8/zad8.js
@@ -0,0 +1,13 @@
+let tab = [12, 67, 34, 23.01, 24, 2, 56, 8, 0*10, 23.02, 78, 34, 45, 67, 98, 1];
+
+for (let i = 0; i < tab.length; i++) {
+    for (let j = 0; j < tab.length; j++) {
+        if (tab[i] < tab[j]) {
+            let x = tab[j];
+            tab[j] = tab[i];
+            tab[i] = x;
+        }
+    }   
+}
+
+console.log(tab)
